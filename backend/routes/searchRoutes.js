9f2b5635943d9a6d2a741c619dcd71846f512ebf
adanfatimaa <adannaqvi786@gmail.com>
2026-08-05 06:35:48 +0500

    minor change in all destinations page

--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -15,7 +15,7 @@ module.exports = router;
 const { getAllCities, addCity, updateCity, deleteCity } = require('../controllers/Citycontroller');
 const { protect, adminOnly } = require('../middleware/authmiddleware');
 
-router.get('/',            protect, adminOnly, getAllCities);
+router.get('/',            getAllCities);
 router.post('/',           protect, adminOnly, addCity);
 router.put('/:id',         protect, adminOnly, updateCity);
 router.delete('/:id',      protect, adminOnly, deleteCity);
